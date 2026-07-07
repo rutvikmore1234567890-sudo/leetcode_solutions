f7871a9da2ba45a97ed4dd58d94b66aba15b5b00
rutvik more <rutvikmore1234567890@gmail.com>
2026-07-07 22:28:47 +0530

    Time: 0 ms (100%), Space: 28.2 MB (67.06%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        int freq = 1;
+        int ans = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                freq++;
+            } else {
+                freq = 1;
+                ans = nums[i];
+            }
+
+            if (freq == n / 2 + 1) {
+                return ans;
+            }
+        }
+
+        return ans;
+    }
+};
