d720c883ff724b298d707fc4a651546263cfadec
rutvik more <rutvikmore1234567890@gmail.com>
2026-07-10 22:18:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -5,7 +5,7 @@ public:
         if(x == 0) return 0.0;
         if(x == 1) return 1.0;
         if(x == -1 && n%2 == 0) return 1.0;
-        if(x == -1 && n % 2 == 0) return 1.0;
+        
         if(x == -1 && n % 2 != 0) return -1.0;
       long   long binForm = n;
         if(n < 0){
